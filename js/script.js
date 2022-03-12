c43430020118fe328b5413b4c76e5435fd0549e3
Irin <iraantonova1998@list.ru>
2022-03-12 10:47:28 +0300

    change click  and add some styles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-const wrapperItems = document.querySelector('.wrapper__items');
-const wrapperItemsKit = document.querySelectorAll('.wrapper__items__kit');
+const wrapperItemsKit = document.querySelectorAll(".wrapper__items__kit");
 
-wrapperItemsKit.forEach((item) => {
-    item.addEventListener('click', () => {
-        item.querySelector('audio').play();
-    })
-})
+for (let i = 0; i < wrapperItemsKit.length; i++) {
+  wrapperItemsKit[i].addEventListener("click", () => {
+    wrapperItemsKit[i].querySelector("audio").play();
+    wrapperItemsKit[i].style.cssText = `
+        transform: translateY(-5px);
+        border: 4px yellow solid;
+        box-shadow: 0px 10px 20px 2px rgba(0,255,255,0.7);`;
+    for (let j = 0; j < wrapperItemsKit.length; j++) {
+      if (i !== j) {
+        wrapperItemsKit[j].style = "";
+      }
+    }
+
+    setTimeout(() =>  wrapperItemsKit[i].style = "", 2000);
+  });
+}
