642ff41eec840fb78217bc9fa1a7c5211ff23675
Irin <iraantonova1998@list.ru>
2022-03-13 17:23:31 +0300

    add event keydown

--- js/script.js
+++ js/script.js
@@ -3,16 +3,31 @@ const wrapperItemsKit = document.querySelectorAll(".wrapper__items__kit");
 for (let i = 0; i < wrapperItemsKit.length; i++) {
   wrapperItemsKit[i].addEventListener("click", () => {
     wrapperItemsKit[i].querySelector("audio").play();
+
+    wrapperItemsKit.forEach((item) => (item.style = ""));
+
     wrapperItemsKit[i].style.cssText = `
         transform: translateY(-5px);
         border: 4px yellow solid;
         box-shadow: 0px 10px 20px 2px rgba(0,255,255,0.7);`;
-    for (let j = 0; j < wrapperItemsKit.length; j++) {
-      if (i !== j) {
-        wrapperItemsKit[j].style = "";
-      }
-    }
 
-    setTimeout(() =>  wrapperItemsKit[i].style = "", 2000);
+    setTimeout(() => (wrapperItemsKit[i].style = ""), 2000);
   });
 }
+
+document.addEventListener("keydown", (e) => {
+  let code = e.code.split("");
+  document.querySelector(`#${code[code.length - 1]}`).play();
+
+  wrapperItemsKit.forEach((item) => {
+    item.style = "";
+  });
+
+  document.querySelector(
+    `#${code[code.length - 1]}`
+  ).parentElement.style.cssText = `
+  transform: translateY(-5px);
+  border: 4px yellow solid;
+  box-shadow: 0px 10px 20px 2px rgba(0,255,255,0.7);
+  `;
+});
